dfa37f1e68554e98b75a686e4eea7185cb29ea2d
Lilia <lilia.manukyan@hotmail.com>
2018-10-03 10:27:24 +0400

    map/filter/reduce

---
+++ map_reduce.py
@@ -0,0 +1,34 @@
+import json
+from functools import reduce
+
+with open('cities.json', 'r') as f:
+    data = json.load(f)
+
+    all_reviews = map(
+        lambda city: list(map(lambda bar: bar['reviews'], city['bars'])), data)
+
+    reviews_per_city = map(
+        lambda city_reviews:
+            list(map(lambda reviews: len(reviews), city_reviews)),
+            all_reviews)
+
+    average_in_cities = map(
+        lambda reviews: reduce(lambda x, y: (x + y) / 2, reviews),
+        list(reviews_per_city))
+
+    average_more_than_9 = filter(lambda avg: avg > 9, average_in_cities)
+
+    print(*average_more_than_9, sep='\n')
+
+    # averages = filter(
+    #     lambda avg: avg >= 9,
+    #     map(
+    #         int,
+    #         map(lambda reviews: reduce(lambda x, y: (x + y) / 2, reviews),
+    #             list(map(
+    #                 lambda city_reviews:
+    #                     list(map(lambda reviews: len(reviews), city_reviews)),
+    #                     map(lambda city: list(map(lambda bar: bar['reviews'],
+    #                         city['bars'])), data))))))
+
+    # print(*averages)
